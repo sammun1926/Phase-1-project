61492d95855d4d0f40416c09e249708e4262bb94
sammun1926 <munorusam10@gmail.com>
2023-04-06 08:38:37 +0300

    added a like counter function

--- src/index.js
+++ src/index.js
@@ -15,4 +15,24 @@ function generateJoke() {
 			jokeText.textContent = 'An error occurred, please try again later.';
 		});
 }
+let likeCounter = 0;
+
+const likeButton = document.querySelector('.heart-btn');
+
+likeButton.addEventListener('click', () => {
+  likeCounter++;
+  document.querySelector('.numb').textContent = likeCounter;
+});
+function generateJoke() {
+	likeCounter = 0;
+	document.querySelector('.numb').textContent = likeCounter;
+	fetch(apiURL)
+		.then(response => response.json())
+		.then(data => {
+			jokeText.textContent = data.value;
+		})
+		.catch(() => {
+			jokeText.textContent = 'An error occurred, please try again later.';
+		});
+}
 
